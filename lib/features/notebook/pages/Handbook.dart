2043d518deb7f17f7d5397a29efac6ed6bd01da9
KHÔI HUỲNH LÊ <23127068@student.hcmus.edu.vn>
2025-07-25 20:47:52 +0700

    Đổi tên file

---
+++ lib/features/notebook/pages/Handbook.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Handbook extends StatelessWidget {
+  const Handbook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drawer Navigation Demo',
+      home: const ,
+    );
+  }
+}
--- lib/features/notebook/pages/notebook_page.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NotebookScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Notebook Screen'));
-  }
-}
